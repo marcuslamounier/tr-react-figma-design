f064f74bd231deb8563dcd8a09b617ff0a92d43a
Marcus <marcus_quadros@hotmail.com>
2022-10-18 18:30:58 -0300

    chore: first push

---
+++ src/components/Heading.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+
+export interface HeadingProps {
+  children: ReactNode;
+  size?: "sm" | "md" | "lg";
+  asChild: boolean;
+}
+
+export function Heading({
+  children,
+  size = "md",
+  asChild = false,
+}: HeadingProps) {
+  const Comp = asChild ? Slot : "h2";
+  return (
+    <Comp
+      className={clsx("text-gray-100 font-bold font-sans", {
+        "text-lg": size === "sm",
+        "text-xl": size === "md",
+        "text-2xl": size === "lg",
+      })}
+    >
+      {children}
+    </Comp>
+  );
+}
